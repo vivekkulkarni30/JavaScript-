78ccf43a200a1b9b3b9f0bf4aa60ae43264ba516
Vivek Kulkarni <vivekkulkarni3013@example.com>
2025-08-08 22:42:17 +0530

    Variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+// In JavaScript, variables are declared using one of three keywords,
+// ie. var, const and let.
+
+const email = "vivek@gmail.com"
+let age = 21;
+var city = "pune";
+
+favHobby = "listening music"; // this is alsom the way to declare the varaiable.
+
+ /* prefer not to use var, 
+ because of issue in block scope and functional-scope */
+
+ // note - if you want to print all the above data in a table the, we can use 
+
+ console.table([email, age, city, favHobby]);
